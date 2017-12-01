13f40538c95712610108432f603b71a8e7425212
chengyanfeng <569498977@qq.com>
2017-12-01 17:09:33 +0800

    完成版本，添加增删改查。

--- src/main/java/controller/OrcleDBConect.java
+++ src/main/java/controller/OrcleDBConect.java
@@ -5,16 +5,18 @@ import java.util.*;
 
 import oracle.jdbc.pool.OracleDataSource;
 
+import static oracle.net.aso.C05.e;
+
 public class OrcleDBConect {
     // 获取数据库连接方法, 返回Connection对象
-    private static Connection con = null;
+    public static Connection con = null;
     //数据执行语句
-    private PreparedStatement ps = null;
-    private ResultSet rs = null;
+    public PreparedStatement ps = null;
+    public ResultSet rs = null;
 
     //创建数据库连接
-    public static Connection con(String sql, Map datamap) {
-        try {
+    public  Connection con(String sql, Map datamap) throws Exception {
+
           /* String username = "OUTLN";
            String password = "123";*/
             OracleDataSource ods = new OracleDataSource();
@@ -26,74 +28,51 @@ public class OrcleDBConect {
             ods.setUser(datamap.get("username").toString()); // username
             ods.setPassword(datamap.get("password").toString()); // password
             con = ods.getConnection();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return con;
+            return con;
     }
 
     //查询
-    public List<Map<String, Object>> add(String sql, Map datamap) throws Exception {
-        Connection conn = con(sql, datamap);
-        PreparedStatement ps = conn.prepareStatement(sql);
-        rs = ps.executeQuery();
-        ResultSetMetaData rsmd = rs.getMetaData();
-        int count = rsmd.getColumnCount();
-        String[] name = new String[count];
-        List<String> list = new ArrayList<String>();
-        for (int i = 0; i < count; i++) {
-            name[i] = rsmd.getColumnName(i + 1);
-            list.add(name[i]);
-            System.out.print(name[i]);
-        }
+    public List<Map<String, Object>> Select(String sql, Map datamap) throws Exception {
+
         List<Map<String, Object>> datelist = new ArrayList<Map<String, Object>>();
-        while (rs.next()) {
-            Map<String, Object> map = new HashMap<String, Object>();
-            for (int i = 0; i < list.size(); i++) {
 
-                Object object = rs.getObject(list.get(i));
-                map.put(list.get(i), object);
-                System.out.print(object);
-            }
-            datelist.add(map);
-        }
+         Connection conn = con(sql, datamap);
+         PreparedStatement ps = conn.prepareStatement(sql);
+         rs = ps.executeQuery();
+         ResultSetMetaData rsmd = rs.getMetaData();
+         int count = rsmd.getColumnCount();
+         String[] name = new String[count];
+         List<String> list = new ArrayList<String>();
+         for (int i = 0; i < count; i++) {
+             name[i] = rsmd.getColumnName(i + 1);
+             list.add(name[i]);
+             System.out.print(name[i]);
+         }
+
+         while (rs.next()) {
+             Map<String, Object> map = new HashMap<String, Object>();
+             for (int i = 0; i < list.size(); i++) {
+
+                 Object object = rs.getObject(list.get(i));
+                 map.put(list.get(i), object);
+                 System.out.print(object);
+             }
+             datelist.add(map);
+
+         }
         return datelist;
+     }
 
-    }
+
 
     //删除，修改，增加
-    public int Update(String sql, Map datamap){
+    public int Update(String sql, Map datamap) throws Exception{
      int i=0;
-      try {
+
           Connection conn = con(sql, datamap);
           PreparedStatement ps = conn.prepareStatement(sql);
            i = ps.executeUpdate(sql);
-      }catch (Exception e){
-          System.out.print(e.toString());
-      }finally {
 
-          if(con!=null){
-              try {
-                  con.close();
-              } catch (SQLException e) {
-                  e.printStackTrace();
-              }
-          }
-          if(rs!=null){
-              try {
-                  rs.close();
-              } catch (SQLException e) {
-                  e.printStackTrace();
-              }
-          }
-          if (ps!=null){
-              try {
-                  ps.close();
-              } catch (SQLException e) {
-                  e.printStackTrace();
-              }
-          }
-      }
         return i;
 
     }
--- src/main/java/controller/WebRounter.java
+++ src/main/java/controller/WebRounter.java
@@ -13,10 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,23 +27,95 @@ public class WebRounter {
 
     @RequestMapping("/")
     String home(@RequestParam(required = false,defaultValue = "") String sql , @RequestParam(required = false,defaultValue = "{\"fmt\":\"null\",\"username\":\"system\",\"password\":\"123456\",\"host\":\"127.0.0.1\",\"name\":\"orcl\"}") String db ) {
-        Map<String,String> map=gson.fromJson(db,HashMap.class);
-        if (sql.isEmpty()){
+        Map<String, String> map = gson.fromJson(db, HashMap.class);
+        if (sql.isEmpty()) {
             return "请填写sql语句";
         }
+        if (sql.contains("SElECT") || sql.contains("select")) {
+            String returndata = Select(sql, map);
+            return returndata;
+        } else {
+            String lg = Updata(sql, map);
+            return lg;
+        }
+
+    }
+
+
+
+
+
+public String Select(String sql,Map map){
+    try {
+        List<Map<String, Object>> returndata = orcleDBConect.Select(sql, map);
+        return gson.toJson(returndata);
+    } catch (Exception e) {
+
+        e.printStackTrace();
+        return e.toString();
+    } finally {
+
+        if (orcleDBConect.con != null) {
+            try {
+                orcleDBConect.con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (orcleDBConect.rs != null) {
+            try {
+                orcleDBConect.rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (orcleDBConect.ps != null) {
+            try {
+                orcleDBConect.ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+}
+
+
+public String Updata(String sql,Map map){
+
+    try {
         int update = orcleDBConect.Update(sql, map);
-
         return gson.toJson(update);
+    } catch (Exception e) {
+        e.printStackTrace();
+        return e.toString();
+    } finally {
 
+        if (orcleDBConect.con != null) {
+            try {
+                orcleDBConect.con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (orcleDBConect.rs != null) {
+            try {
+                orcleDBConect.rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (orcleDBConect.ps != null) {
+            try {
+                orcleDBConect.ps.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
-    @RequestMapping("/hello/{myName}")
-    String index(@PathVariable String myName) {
-        return "Hello "+myName+"!!!";
-    }
-
-
-
+}
 
 
 
